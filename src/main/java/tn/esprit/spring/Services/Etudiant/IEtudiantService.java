6393a940d97f0c5621b33ccca2ae26a040531830
omar <omar.jawedi@esprit.tn>
2024-11-05 02:15:52 +0100

    devb11

--- src/main/java/tn/esprit/spring/Services/Etudiant/IEtudiantService.java
+++ src/main/java/tn/esprit/spring/Services/Etudiant/IEtudiantService.java
@@ -1,4 +1,4 @@
-package tn.esprit.spring;
+package tn.esprit.spring.Services.Etudiant;
 
 import tn.esprit.spring.DAO.Entities.Etudiant;
 
--- src/main/java/tn/esprit/spring/Services/Foyer/IFoyerService.java
+++ src/main/java/tn/esprit/spring/Services/Foyer/IFoyerService.java
@@ -1,4 +1,4 @@
-package tn.esprit.spring;
+package tn.esprit.spring.Services.Foyer;
 
 import tn.esprit.spring.DAO.Entities.Etudiant;
 import tn.esprit.spring.DAO.Entities.Foyer;
